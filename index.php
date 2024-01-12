85b9438477e800a83273c076e8b3233665263441
GiuliaCarangelo <tnhkagura@gmail.com>
2024-01-12 19:20:28 +0100

    add input to censor word

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo "Censor a random word! :D " . "<br>";
+
+$paragr = "She waits, arms folded.  A small black-and white TV next to her register shows George Bush on the White House lawn with helicopter rotors spinning behind him.";
+
+echo str_replace($_GET["censoredWord"],"***",$paragr);
+
+?>
+
+<form action="./index.php" method="GET">
+    <label for="wordToCensor">Type the word to censor:</label>
+    <input type="text" name="censoredWord" id="censoredWord">
+    <button type="submit">Censor</button>
+</form>
+
+
+
